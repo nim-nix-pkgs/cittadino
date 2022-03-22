7314d80b60d9230652c8ece425ebc7f4e52963d6
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:09 -0300

    chore: re-index ../nimpkgs/c/cittadino/0_1_4

--- 0_1_4/flake.nix
+++ 0_1_4/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-cittadino-0_1_4.flake = false;
   inputs.src-cittadino-0_1_4.owner = "makingspace";
-  inputs.src-cittadino-0_1_4.ref   = "refs/tags/0.1.4";
+  inputs.src-cittadino-0_1_4.ref   = "0_1_4";
   inputs.src-cittadino-0_1_4.repo  = "cittadino";
   inputs.src-cittadino-0_1_4.type  = "github";
   
